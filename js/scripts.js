1f500bc7fd7d91800cd39b4b3d8bcf610bf18f77
aselekoglu <aselekoglu97@gmail.com>
2021-12-06 16:56:48 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+let clipboard = new ClipboardJS(".btn");
+
+var searchBlock = document.getElementById('output');
+
+
+function generateSig(){
+
+    var name = document.getElementById('full-name').value,
+        title = document.getElementById('title').value,
+        email = document.getElementById('email').value,
+        phone = document.getElementById('phone').value,
+        photo_id = document.getElementById('photo-id').value;
+
+    console.log(name,title,email,phone,photo_id,searchBlock);
+
+    $(searchBlock).find('#sign-name').text(name);
+    $(searchBlock).find('#sign-title').text(title);
+    $(searchBlock).find('#sign-email-vis').text(email);
+    $(searchBlock).find('#sign-email-href').attr('href','mailto:'+email);
+    $(searchBlock).find('#sign-mobile-vis').text(phone);
+    $(searchBlock).find('#sign-mobile-href').attr('href','tel:'+phone);
+
+    $(searchBlock).find('#photo').attr('src', 'https://drive.google.com/uc?id='+photo_id);
+    $('button').show();
+}
+clipboard.on('success', function(e) {
+    $('#js-copy').val('Successfully copied!');
+});
+
+clipboard.on('error', function(e) {
+    $('#js-copy').val('Could not be copied. See console for the error log.');
+    console.log(e);
+});
