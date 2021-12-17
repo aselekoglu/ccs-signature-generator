90da316bd6fd77806198636958142ef854722ad1
aselekoglu <aselekoglu97@gmail.com>
2021-12-17 16:11:11 +0300

    instructions, fixes, .css improvements etc.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 let clipboard = new ClipboardJS(".btn");
 
 var searchBlock = document.getElementById('output');
+var links = document.getElementsByClassName('sc-hzDkRC')
 
-
-function generateSig(){
+function generateSig() {
 
     var name = document.getElementById('full-name').value,
         title = document.getElementById('title').value,
@@ -11,23 +11,34 @@ function generateSig(){
         phone = document.getElementById('phone').value,
         photo_id = document.getElementById('photo-id').value;
 
-    console.log(name,title,email,phone,photo_id,searchBlock);
+    console.log(name, title, email, phone, photo_id, searchBlock);
 
     $(searchBlock).find('#sign-name').text(name);
     $(searchBlock).find('#sign-title').text(title);
     $(searchBlock).find('#sign-email-vis').text(email);
-    $(searchBlock).find('#sign-email-href').attr('href','mailto:'+email);
+    $(searchBlock).find('#sign-email-href').attr('href', 'mailto:' + email);
     $(searchBlock).find('#sign-mobile-vis').text(phone);
-    $(searchBlock).find('#sign-mobile-href').attr('href','tel:'+phone);
+    $(searchBlock).find('#sign-mobile-href').attr('href', 'tel:' + phone);
+    $(searchBlock).find('#photo').attr('src', 'https://drive.google.com/uc?id=' + photo_id);
 
-    $(searchBlock).find('#photo').attr('src', 'https://drive.google.com/uc?id='+photo_id);
+    $(links).each(function () {
+        var $this = $(this);
+        var _href = $this.attr("href");
+        $this.attr("href", _href + email)
+    })
+
+    console.log($(links).attr("href"))
+
     $('button').show();
 }
-clipboard.on('success', function(e) {
+
+clipboard.on('success', function (e) {
     $('#js-copy').val('Successfully copied!');
 });
 
-clipboard.on('error', function(e) {
+clipboard.on('error', function (e) {
     $('#js-copy').val('Could not be copied. See console for the error log.');
     console.log(e);
 });
+
+
